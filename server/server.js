2f42b743c4e1718d666193fe79f4a3ba3a0ef457
Pragat Sharma <2020csb1109@iitrpr.ac.in>
2025-06-29 10:23:11 +0530

    Backend running succesful

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// server/server.js
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+dotenv.config();       // Load .env file
+connectDB();           // Connect to MongoDB
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // To parse JSON bodies
+
+app.get("/", (req, res) => {
+  res.send("Knowledge Base Platform API is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
